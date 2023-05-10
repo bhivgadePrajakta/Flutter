d04cfc3a0b0af73555bdd156b92771526b0f117c
Prajakta <prajaktabhivagade@gmail.com>
2023-05-10 22:56:07 +0530

    Day 1 completed

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:first_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
