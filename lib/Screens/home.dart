1222c9c607c3367c72009c5702306f644baa88a3
Prajakta <prajaktabhivagade@gmail.com>
2023-05-12 19:11:54 +0530

    Day 3 completed

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   final int day = 30;
   final String name = "flutter";
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,27 @@
 import 'package:first_app/Screens/home.dart';
 import 'package:first_app/Screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+          primarySwatch: Colors.red, fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
-        "/": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/": (context) => LoginPage(),
+        "/home": (context) => const HomePage()
       },
     );
   }
